f87fbde08964200a05d52e613ffbd980f3536db5
Princeton D'souza <princeton2102@gmail.com>
2022-11-20 17:09:42 +0530

    added rank page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Card from './components/Card/Card'
-import Form from './components/Form/Form'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Compare from "./pages/Compare/Compare";
+import Rank from "./pages/Rank/Rank";
+import React from "react";
+import "./App.css";
 function App() {
-  
-
   return (
-    <div className="App">
-    <Form/>
-    <Card/>
-      
-    </div>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/compare" element={<Compare />} />
+        <Route path="/rank" element={<Rank/>} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
