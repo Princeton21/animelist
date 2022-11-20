f55904de243d063a4aa1d0d14997932d959c20a7
Princeton D'souza <princeton2102@gmail.com>
2022-11-20 17:09:56 +0530

    added data and assets

---
+++ src/assets/Data.js
@@ -0,0 +1,40 @@
+const img1 =
+  "https://th.bing.com/th/id/OIP.Se6qtSnKfeDlIIUSrtgZ3gHaLs?pid=ImgDet&rs=1";
+export const cardData = [
+  {
+    rank: "#1",
+    img: img1,
+    title: "Chainsaw Man",
+    counter: 4,
+  },
+  {
+    rank: "#2",
+    img: img1,
+    title: "SpyXFamily",
+    counter: 1,
+  },
+  {
+    rank: "#3",
+    img: img1,
+    title: "One Piece",
+    counter: 8,
+  },
+  {
+    rank: "#4",
+    img: img1,
+    title: "Tokyo Ghoul",
+    counter: 10,
+  },
+  {
+    rank: "#5",
+    img: img1,
+    title: "Attack on Titan",
+    counter: 2,
+  },
+  {
+    rank: "#6",
+    img: img1,
+    title: "My Hero Academia",
+    counter: 20,
+  },
+];
